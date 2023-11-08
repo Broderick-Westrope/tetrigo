6201eb0b8466c960181255cd6544af8c271d0817
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-08 16:02:13 +1100

    feat: add remainder of key map

--- keys.go
+++ keys.go
@@ -3,11 +3,25 @@ package main
 import "github.com/charmbracelet/bubbles/key"
 
 type KeyMap struct {
-	Quit key.Binding
+	Quit             key.Binding
+	Help             key.Binding
+	Left             key.Binding
+	Right            key.Binding
+	Clockwise        key.Binding
+	CounterClockwise key.Binding
+	SoftDrop         key.Binding
+	HardDrop         key.Binding
 }
 
 func DefaultKeyMap() *KeyMap {
 	return &KeyMap{
-		Quit: key.NewBinding(key.WithKeys("q", "ctrl+c"), key.WithHelp("q", "quit")),
+		Quit:             key.NewBinding(key.WithKeys("q", "ctrl+c"), key.WithHelp("q", "quit")),
+		Help:             key.NewBinding(key.WithKeys("?"), key.WithHelp("?", "help")),
+		Left:             key.NewBinding(key.WithKeys("left", "j", "a"), key.WithHelp("a, j, left", "move left")),
+		Right:            key.NewBinding(key.WithKeys("right", "l", "d"), key.WithHelp("d, l, right", "move right")),
+		Clockwise:        key.NewBinding(key.WithKeys(">", "e", "o"), key.WithHelp("e, o, >", "rotate clockwise")),
+		CounterClockwise: key.NewBinding(key.WithKeys("<", "q", "u"), key.WithHelp("q, u, <", "rotate counter-clockwise")),
+		SoftDrop:         key.NewBinding(key.WithKeys("down", "s", "k"), key.WithHelp("s, k, down", "soft drop")),
+		HardDrop:         key.NewBinding(key.WithKeys("up", "w", "i"), key.WithHelp("w, i, up", "hard drop")),
 	}
 }
