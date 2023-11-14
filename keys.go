8faa6418a7a739f6310363b334db3a50d90b7c32
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-14 12:24:29 +1100

    chore: remove arrows from key map; add hold to key map

--- keys.go
+++ keys.go
@@ -11,18 +11,20 @@ type KeyMap struct {
 	CounterClockwise key.Binding
 	SoftDrop         key.Binding
 	HardDrop         key.Binding
+	Hold             key.Binding
 }
 
 func DefaultKeyMap() *KeyMap {
 	return &KeyMap{
 		Quit:             key.NewBinding(key.WithKeys("esc", "ctrl+c"), key.WithHelp("esc", "quit")),
 		Help:             key.NewBinding(key.WithKeys("?"), key.WithHelp("?", "help")),
-		Left:             key.NewBinding(key.WithKeys("left", "j", "a"), key.WithHelp("a, j, left", "move left")),
-		Right:            key.NewBinding(key.WithKeys("right", "l", "d"), key.WithHelp("d, l, right", "move right")),
-		Clockwise:        key.NewBinding(key.WithKeys(">", "e", "o"), key.WithHelp("e, o, >", "rotate clockwise")),
-		CounterClockwise: key.NewBinding(key.WithKeys("<", "q", "u"), key.WithHelp("q, u, <", "rotate counter-clockwise")),
-		SoftDrop:         key.NewBinding(key.WithKeys("down", "s", "k"), key.WithHelp("s, k, down", "soft drop")),
-		HardDrop:         key.NewBinding(key.WithKeys("up", "w", "i"), key.WithHelp("w, i, up", "hard drop")),
+		Left:             key.NewBinding(key.WithKeys("j", "a"), key.WithHelp("a, j", "move left")),
+		Right:            key.NewBinding(key.WithKeys("l", "d"), key.WithHelp("d, l", "move right")),
+		Clockwise:        key.NewBinding(key.WithKeys("e", "o"), key.WithHelp("e, o", "rotate clockwise")),
+		CounterClockwise: key.NewBinding(key.WithKeys("q", "u"), key.WithHelp("q, u", "rotate counter-clockwise")),
+		SoftDrop:         key.NewBinding(key.WithKeys("s", "k"), key.WithHelp("s, k", "toggle soft drop")),
+		HardDrop:         key.NewBinding(key.WithKeys("w", "i"), key.WithHelp("w, i", "hard drop")),
+		Hold:             key.NewBinding(key.WithKeys("space"), key.WithHelp("space", "hold")),
 	}
 }
 
@@ -38,18 +40,17 @@ func (k *KeyMap) FullHelp() [][]key.Binding {
 		{
 			k.Quit,
 			k.Help,
-		},
-		{
 			k.Left,
-			k.Right,
 		},
 		{
+			k.Right,
 			k.Clockwise,
 			k.CounterClockwise,
 		},
 		{
 			k.SoftDrop,
 			k.HardDrop,
+			k.Hold,
 		},
 	}
 }
