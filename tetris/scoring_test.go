d5128b7e0f1a389648faa9571fce42771ff8aa1c
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-15 19:26:13 +1100

    test: add TestScoring_ProcessAction

--- tetris/scoring_test.go
+++ tetris/scoring_test.go
@@ -120,3 +120,202 @@ func TestScoring_Lines(t *testing.T) {
 		})
 	}
 }
+
+func TestScoring_ProcessAction(t *testing.T) {
+	// Each action should have 2 test cases: one with back-to-back enabled, and one without.
+	tt := []struct {
+		name               string
+		a                  action
+		isBackToBack       bool
+		expectedTotal      uint
+		expectedBackToBack bool
+	}{
+		// Back-to-back disabled
+		{
+			name:               "no action, no back to back",
+			a:                  actionNone,
+			isBackToBack:       false,
+			expectedTotal:      0,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "single, no back to back",
+			a:                  actionSingle,
+			isBackToBack:       false,
+			expectedTotal:      100,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "double, no back to back",
+			a:                  actionDouble,
+			isBackToBack:       false,
+			expectedTotal:      300,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "triple, no back to back",
+			a:                  actionTriple,
+			isBackToBack:       false,
+			expectedTotal:      500,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "tetris, no back to back",
+			a:                  actionTetris,
+			isBackToBack:       false,
+			expectedTotal:      800,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "mini T-spin, no back to back",
+			a:                  actionMiniTSpin,
+			isBackToBack:       false,
+			expectedTotal:      100,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "mini T-spin single, no back to back",
+			a:                  actionMiniTSpinSingle,
+			isBackToBack:       false,
+			expectedTotal:      200,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin, no back to back",
+			a:                  actionTSpin,
+			isBackToBack:       false,
+			expectedTotal:      400,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "T-spin single, no back to back",
+			a:                  actionTSpinSingle,
+			isBackToBack:       false,
+			expectedTotal:      800,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin double, no back to back",
+			a:                  actionTSpinDouble,
+			isBackToBack:       false,
+			expectedTotal:      1200,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin triple, no back to back",
+			a:                  actionTSpinTriple,
+			isBackToBack:       false,
+			expectedTotal:      1600,
+			expectedBackToBack: true,
+		},
+		// Back-to-back enabled
+		{
+			name:               "no action, back to back",
+			a:                  actionNone,
+			isBackToBack:       true,
+			expectedTotal:      0,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "single, back to back",
+			a:                  actionSingle,
+			isBackToBack:       true,
+			expectedTotal:      100,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "double, back to back",
+			a:                  actionDouble,
+			isBackToBack:       true,
+			expectedTotal:      300,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "triple, back to back",
+			a:                  actionTriple,
+			isBackToBack:       true,
+			expectedTotal:      500,
+			expectedBackToBack: false,
+		},
+		{
+			name:               "tetris, back to back",
+			a:                  actionTetris,
+			isBackToBack:       true,
+			expectedTotal:      800 * 1.5,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "mini T-spin, back to back",
+			a:                  actionMiniTSpin,
+			isBackToBack:       true,
+			expectedTotal:      100,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "mini T-spin single, back to back",
+			a:                  actionMiniTSpinSingle,
+			isBackToBack:       true,
+			expectedTotal:      200 * 1.5,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin, back to back",
+			a:                  actionTSpin,
+			isBackToBack:       true,
+			expectedTotal:      400,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin single, back to back",
+			a:                  actionTSpinSingle,
+			isBackToBack:       true,
+			expectedTotal:      800 * 1.5,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin double, back to back",
+			a:                  actionTSpinDouble,
+			isBackToBack:       true,
+			expectedTotal:      1200 * 1.5,
+			expectedBackToBack: true,
+		},
+		{
+			name:               "T-spin triple, back to back",
+			a:                  actionTSpinTriple,
+			isBackToBack:       true,
+			expectedTotal:      1600 * 1.5,
+			expectedBackToBack: true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			s := &Scoring{
+				backToBack: tc.isBackToBack,
+				total:      0,
+				lines:      0,
+				level:      1,
+			}
+
+			s.ProcessAction(tc.a)
+
+			if s.total != tc.expectedTotal {
+				t.Errorf("Total: expected %d, got %d", tc.expectedTotal, s.total)
+			}
+
+			if s.backToBack != tc.expectedBackToBack {
+				t.Errorf("BackToBack: expected %t, got %t", tc.expectedBackToBack, s.backToBack)
+			}
+
+			expectedLines := (tc.expectedTotal / 100)
+			if s.lines != expectedLines {
+				t.Errorf("Lines: expected %d, got %d", expectedLines, s.lines)
+			}
+
+			expectedLevel := 1 + uint(expectedLines/5)
+			if s.level != expectedLevel {
+				t.Errorf("Level: expected %d, got %d", expectedLevel, s.level)
+			}
+		})
+	}
+}
