437517598f059c1953fee96004e9f723fba1988a
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-15 19:47:39 +1100

    test: add TestMatrix_RemoveLine

--- tetris/matrix_test.go
+++ tetris/matrix_test.go
@@ -1,6 +1,9 @@
 package tetris
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMatrix_IsLineComplete(t *testing.T) {
 	m := &Matrix{
@@ -29,6 +32,36 @@ func TestMatrix_IsLineComplete(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			if actual := m.isLineComplete(tcIndex); actual != tc.expected {
 				t.Errorf("expected %v, got %v", tc.expected, actual)
+			}
+		})
+	}
+}
+
+func TestMatrix_RemoveLine(t *testing.T) {
+	testRows := Matrix{
+		[10]byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		[10]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		[10]byte{0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		[10]byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+		[10]byte{1, 1, 1, 1, 0, 1, 1, 0, 0, 1},
+		[10]byte{1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+	}
+
+	for rowIndex := range testRows {
+		t.Run(fmt.Sprint("row ", rowIndex), func(t *testing.T) {
+			m := Matrix{}
+			m[rowIndex] = testRows[rowIndex]
+
+			if m[rowIndex] != testRows[rowIndex] {
+				t.Errorf("Before: expected %v, got %v", testRows[rowIndex], m[rowIndex])
+			}
+
+			m.removeLine(rowIndex)
+
+			for colIndex := range m[rowIndex] {
+				if m[rowIndex][colIndex] != 0 {
+					t.Errorf("After: expected 0, got %v", m[rowIndex][colIndex])
+				}
 			}
 		})
 	}
