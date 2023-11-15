20e127094dc654b59262942cde994bb62d1a11b8
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-15 16:40:57 +1100

    test: add TestBag_Next

--- tetris/bag_test.go
+++ tetris/bag_test.go
@@ -1,7 +1,77 @@
 package tetris
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
+// Checks:
+//   - that the tetrimino returned is the first element of the bag.
+//   - that the first element of the bag is removed.
+//   - that the bag is filled with 7 tetriminos if it has less than 7 elements.
+func TestBag_Next(t *testing.T) {
+	tt := []struct {
+		name     string
+		elements []Tetrimino
+	}{
+		{
+			name:     "2 elements",
+			elements: make([]Tetrimino, 2, 14),
+		},
+		{
+			name:     "7 elements",
+			elements: make([]Tetrimino, 7, 14),
+		},
+		{
+			name:     "8 elements",
+			elements: make([]Tetrimino, 8, 14),
+		},
+		{
+			name:     "14 elements",
+			elements: make([]Tetrimino, 14),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			b := Bag{
+				Elements:     tc.elements,
+				matrixHeight: 40,
+			}
+			expected := tc.elements[0].Copy()
+			expected.Pos.Y += b.matrixHeight - 20
+
+			var expectedElements []Tetrimino
+			for _, e := range tc.elements[1:] {
+				temp := e.Copy()
+				temp.Pos.Y += b.matrixHeight - 20
+				expectedElements = append(expectedElements, *temp)
+			}
+
+			result := b.Next()
+			if !reflect.DeepEqual(*result, *expected) {
+				t.Errorf("Tetrimino: want %v, got %v", *expected, *result)
+			}
+
+			for i := range b.Elements {
+				b.Elements[i].Pos.Y += b.matrixHeight - 20
+			}
+
+			if v := b.Elements[:len(expectedElements)]; !reflect.DeepEqual(v, expectedElements) {
+				t.Errorf("Elements: want %v, got %v", expectedElements, v)
+			}
+
+			expectedLength := len(expectedElements)
+			if expectedLength < 7 && len(b.Elements) != expectedLength+7 {
+				t.Errorf("Length: want %d, got %d", expectedLength+7, len(b.Elements))
+			}
+		})
+	}
+}
+
+// Checks:
+//   - that the bag is filled with 7 tetriminos if it has less than 7 elements.
+//   - that the tetriminos added to the bag are not duplicates.
 func TestBag_Fill(t *testing.T) {
 	tt := []struct {
 		name        string
