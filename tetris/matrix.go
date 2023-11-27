6320ca711070358983b53181d0a064c50f85f8df
Broderick Westrope <broderickwestrope@gmail.com>
2023-11-27 19:31:06 +1100

    refactor: fix matrix naming

--- tetris/matrix.go
+++ tetris/matrix.go
@@ -4,8 +4,8 @@ import "fmt"
 
 type Matrix [40][10]byte
 
-func (p *Matrix) isLineComplete(row int) bool {
-	for _, cell := range p[row] {
+func (m *Matrix) isLineComplete(row int) bool {
+	for _, cell := range m[row] {
 		if isCellEmpty(cell) {
 			return false
 		}
@@ -13,63 +13,63 @@ func (p *Matrix) isLineComplete(row int) bool {
 	return true
 }
 
-func (p *Matrix) removeLine(row int) {
-	p[0] = [10]byte{}
+func (m *Matrix) removeLine(row int) {
+	m[0] = [10]byte{}
 	for i := row; i > 0; i-- {
-		p[i] = p[i-1]
+		m[i] = m[i-1]
 	}
 }
 
-func (p *Matrix) RemoveTetrimino(tetrimino *Tetrimino) error {
+func (m *Matrix) RemoveTetrimino(tetrimino *Tetrimino) error {
 	for row := range tetrimino.Cells {
 		for col := range tetrimino.Cells[row] {
 			if tetrimino.Cells[row][col] {
 				// The following bounds checks should never be reached as long as tetriminos are added with the dedicated function
-				if row+tetrimino.Pos.Y >= len(p) || row+tetrimino.Pos.Y < 0 {
+				if row+tetrimino.Pos.Y >= len(m) || row+tetrimino.Pos.Y < 0 {
 					return fmt.Errorf("row %d is out of bounds", row+tetrimino.Pos.Y)
 				}
-				if col+tetrimino.Pos.X >= len(p[row]) || col+tetrimino.Pos.X < 0 {
+				if col+tetrimino.Pos.X >= len(m[row]) || col+tetrimino.Pos.X < 0 {
 					return fmt.Errorf("col %d is out of bounds", col+tetrimino.Pos.X)
 				}
 
-				cellValue := p[row+tetrimino.Pos.Y][col+tetrimino.Pos.X]
+				cellValue := m[row+tetrimino.Pos.Y][col+tetrimino.Pos.X]
 				if cellValue != tetrimino.Value {
 					return fmt.Errorf("cell at row %d, col %d is '%s' (byte value %v) not the expected '%s' (byte value %v)", row+tetrimino.Pos.Y, col+tetrimino.Pos.X, string(cellValue), cellValue, string(tetrimino.Value), tetrimino.Value)
 				}
-				p[row+tetrimino.Pos.Y][col+tetrimino.Pos.X] = 0
+				m[row+tetrimino.Pos.Y][col+tetrimino.Pos.X] = 0
 			}
 		}
 	}
 	return nil
 }
 
-func (p *Matrix) AddTetrimino(tetrimino *Tetrimino) error {
+func (m *Matrix) AddTetrimino(tetrimino *Tetrimino) error {
 	for row := range tetrimino.Cells {
 		for col := range tetrimino.Cells[row] {
 			if tetrimino.Cells[row][col] {
-				if row+tetrimino.Pos.Y >= len(p) || row+tetrimino.Pos.Y < 0 {
+				if row+tetrimino.Pos.Y >= len(m) || row+tetrimino.Pos.Y < 0 {
 					return fmt.Errorf("row %d is out of bounds", row+tetrimino.Pos.Y)
 				}
-				if col+tetrimino.Pos.X >= len(p[row]) || col+tetrimino.Pos.X < 0 {
+				if col+tetrimino.Pos.X >= len(m[row]) || col+tetrimino.Pos.X < 0 {
 					return fmt.Errorf("col %d is out of bounds", col+tetrimino.Pos.X)
 				}
 
-				cellValue := p[row+tetrimino.Pos.Y][col+tetrimino.Pos.X]
+				cellValue := m[row+tetrimino.Pos.Y][col+tetrimino.Pos.X]
 				if !isCellEmpty(cellValue) {
 					return fmt.Errorf("cell at row %d, col %d is '%s' (byte value %v) not empty", row+tetrimino.Pos.Y, col+tetrimino.Pos.X, string(cellValue), cellValue)
 				}
-				p[row+tetrimino.Pos.Y][col+tetrimino.Pos.X] = tetrimino.Value
+				m[row+tetrimino.Pos.Y][col+tetrimino.Pos.X] = tetrimino.Value
 			}
 		}
 	}
 	return nil
 }
 
-func (p *Matrix) RemoveCompletedLines(tet *Tetrimino) action {
+func (m *Matrix) RemoveCompletedLines(tet *Tetrimino) action {
 	lines := 0
 	for row := range tet.Cells {
-		if p.isLineComplete(tet.Pos.Y + row) {
-			p.removeLine(tet.Pos.Y + row)
+		if m.isLineComplete(tet.Pos.Y + row) {
+			m.removeLine(tet.Pos.Y + row)
 			lines++
 		}
 	}
