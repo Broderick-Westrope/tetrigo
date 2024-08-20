23cf7f6660b11702e420be7a79c7a38a9bc70f58
Broderick Westrope <broderickwestrope@gmail.com>
2024-08-20 14:27:42 +1000

    chore: fix lint

--- cmd/tetrigo/main.go
+++ cmd/tetrigo/main.go
@@ -41,8 +41,9 @@ func (c *PlayCmd) Run(globals *GlobalVars) error {
 	switch c.GameMode {
 	case "marathon":
 		return launchStarter(globals, common.ModeMarathon, common.NewMarathonInput(c.Level, c.Name))
+	default:
+		return fmt.Errorf("invalid game mode: %s", c.GameMode)
 	}
-	return fmt.Errorf("invalid game mode: %s", c.GameMode)
 }
 
 type LeaderboardCmd struct {
