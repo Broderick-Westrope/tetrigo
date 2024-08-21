893bb1bf9ea931d9fc67510070a2556837149e70
Broderick Westrope <broderickwestrope@gmail.com>
2024-08-21 21:10:58 +1000

    ci: specify bash for release scripts (#14)
    * ci: specify bash for release scripts
    
    * ci: update test workflow matrix

--- scripts/goreleaser/dryrun.sh
+++ scripts/goreleaser/dryrun.sh
@@ -1,11 +1,9 @@
-#!/bin/zsh
-
 docker run \
 --rm \
 -e CGO_ENABLED=1 \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v `pwd`:/go/src/"$PACKAGE_NAME" \
--v `pwd`/sysroot:/sysroot \
+-v "$(pwd)":/go/src/"$PACKAGE_NAME" \
+-v "$(pwd)"/sysroot:/sysroot \
 -w /go/src/"$PACKAGE_NAME" \
 ghcr.io/goreleaser/goreleaser-cross:"$GOLANG_CROSS_VERSION" \
 --clean --skip=validate --skip=publish
